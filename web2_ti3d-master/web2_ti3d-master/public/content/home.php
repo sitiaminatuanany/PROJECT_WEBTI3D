3799d8226cd3bcc943c99e57c9c84e3be5f5a08b
sitiaminatuanany <125535122+sitiaminatuanany@users.noreply.github.com>
2023-02-15 23:13:27 -0800

    Add files via upload

---
+++ web2_ti3d-master/web2_ti3d-master/public/content/home.php
@@ -0,0 +1,9 @@
+<div class="col-8">
+    <div class="card">
+        <div class="card-body">
+            <h1>HOME</h1>
+            Selamat Datang <?= $user; ?>, anda login menggunakan level <?= $level ?>
+        </div>
+    </div>
+</div>
+
