b347910724407ed49ecab0527116e9d2a80a6e32
sitiaminatuanany <125535122+sitiaminatuanany@users.noreply.github.com>
2023-02-15 23:04:41 -0800

    Delete web2_ti3d-master directory

--- web2_ti3d-master/config/menu.php
+++
@@ -1,6 +0,0 @@
-<div class="menu">
-    <a href="index.php">Home</a>
-    <a href="index.php?page=profil">Profil</a>
-    <a href="index.php?page=kontak">Kontak</a>
-    <a href="index.php?page=galeri">Galeri</a>
-</div>
--- web2_ti3d-master/public/content/404.php
+++
@@ -1,6 +0,0 @@
-<div class="row">
-    <div class="col-12">
-        <h1>404</h1>
-        <h3>Page Not Found</h3>
-    </div>
-</div>
